6b41d25b2b0c82740152695718f654366b80b855
King3David21 <168435457+King3David21@users.noreply.github.com>
2024-05-28 22:27:03 -0400

    Level Button added

--- Menu.gd
+++ Menu.gd
@@ -1,10 +1,11 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://World/Levels/level 1.tscn")
+	get_tree().change_scene_to_file("res://World/Levels/level_1.tscn")
 
 func _on_levels_pressed():
 	get_tree().change_scene_to_file("res://menu/levels_menu.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
+
--- menu/levels_menu.gd
+++ menu/levels_menu.gd
@@ -4,3 +4,15 @@ extends Control
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://menu/menu.tscn")
+
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://World/Levels/level_1.tscn")
+
+
+func _on_level_2_pressed():
+	get_tree().change_scene_to_file("res://World/Levels/level_2.tscn")
+
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://World/Levels/level_3.tscn")
